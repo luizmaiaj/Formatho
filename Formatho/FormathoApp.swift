661d22b9e74f8977f5ab9f21103b41c66e2011ce
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2022-09-11 21:44:18 +0700

    Initial Commit

---
+++ Formatho/FormathoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormathoApp.swift
+//  Formatho
+//
+//  Created by Luiz Carlos Maia Junior on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FormathoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
