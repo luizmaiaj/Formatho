0c740ae5aa086694c576d8f5da2791a71b425505
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2022-09-21 01:15:55 +0000

    1- Moved main view into a separate file to start refactoring

---
+++ Formatho/Main.swift
@@ -0,0 +1,107 @@
+//
+//  Main.swift
+//  Formatho
+//
+//  Created by Luiz Carlos Maia Junior on 21/9/22.
+//
+
+import SwiftUI
+
+import Combine // for Just
+
+struct Main: View {
+    
+    @ObservedObject var fetcher: Fetcher
+    
+    @AppStorage("email") private var email: String = String()
+    @AppStorage("pat") private var pat: String = String()
+    
+    @State var witid: String = String()
+    
+    @State var isPresentedLogin = false
+    
+    var body: some View {
+        VStack {
+            
+            if isPresentedLogin {
+                Form {
+                    TextField("email", text: $email)
+                    
+                    TextField("PAT", text: $pat)
+                }
+            }
+            
+            Form {
+                Button("Get projects", action: {
+                    fetcher.projects(pat: pat, email: email)
+                })
+                
+                HStack {
+                    Text("WIT ID")
+                        .frame(width: 50, alignment: .leading)
+                    
+                    TextField("> 50001", text: $witid)
+                        .frame(alignment: .trailing)
+                        .onReceive(Just(witid)) { newValue in
+                            let filtered = newValue.filter { "0123456789".contains($0) }
+                            if filtered != newValue { witid = filtered }
+                        }
+                        .onSubmit {
+                            fetcher.wits(pat: pat, email: email, witid: witid)
+                        }
+                    
+                    Button("Get WIT", action: {
+                        fetcher.wits(pat: pat, email: email, witid: witid)
+                    })
+                }
+            }
+            .frame(minWidth: 300)
+            
+            if fetcher.isLoading {
+                
+                Text("Fetching...")
+                
+            } else if fetcher.errorMessage != nil {
+                
+                Text(self.fetcher.errorMessage ?? "")
+                
+            } else if fetcher.wits.count > 0 {
+                
+                HStack{
+                    Text("Copied to clipboard:")
+                        .font(.body)
+                    
+                    Text(fetcher.formattedWIT)
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem {
+                
+                Button {
+                    isPresentedLogin.toggle()
+                } label: {
+                    HStack {
+                        
+                        if email.isEmpty {
+                            Text("Login details")
+                        } else {
+                            Text(email)
+                        }
+                        
+                        Image(systemName: "person.circle")
+                            .font(.title2)
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct Main_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        Main(fetcher: Fetcher())
+    }
+}
