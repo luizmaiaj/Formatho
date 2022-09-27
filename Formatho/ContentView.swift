b161ff3d203ac96f4babcc9654f09a38191f0e01
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2022-09-27 08:45:44 +0700

    1- Reduced the minimum app height to match what it was in build 7

--- Formatho/ContentView.swift
+++ Formatho/ContentView.swift
@@ -69,7 +69,7 @@ struct ContentView: View {
                 }
             }
         }
-        .frame(minWidth: 500, maxWidth: 1000, minHeight: 300, maxHeight: 1000)
+        .frame(minWidth: 500, maxWidth: 1000, minHeight: 100, maxHeight: 1000)
     }
 }
 
