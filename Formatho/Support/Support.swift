40179b8f8566fb192f5e0210f80c677335b3e3a7
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2022-09-30 15:46:32 +0000

    1- Changed toRTF extension function of string to add a font type body attribute to normalise the report text being displayed in the GridView

--- Formatho/Support/Support.swift
+++ Formatho/Support/Support.swift
@@ -5,15 +5,17 @@
 //  Created by Luiz Carlos Maia Junior on 30/9/22.
 //
 
-import Foundation
-import AppKit
+import SwiftUI
 
 extension String {
     func toRTF() -> AttributedString {
+                
         if let data = self.data(using: .unicode),
-           var nsAttrString = try? NSMutableAttributedString(data: data,
+           let nsAttrString = try? NSMutableAttributedString(data: data,
                                                       options: [.documentType: NSAttributedString.DocumentType.html],
                                                       documentAttributes: nil) {
+            
+            nsAttrString.addAttribute(.font, value: Font.body, range: NSRange(location: 0, length: nsAttrString.length))
             
             return AttributedString(nsAttrString) // string to be displayed in Text()
         }
