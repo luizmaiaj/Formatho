98e844299ddf36a818e07c3969ccd78013b07761
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2022-10-13 18:55:22 +0700

    1- Removed getRelations from Fetcher

--- Formatho/Fetcher.swift
+++ Formatho/Fetcher.swift
@@ -11,8 +11,6 @@ import AppKit // for clipboard access
 
 class Fetcher: ObservableObject {
     
-    @AppStorage("fetched") private var fetched: [String] = [String]()
-    
     @Published var projects: [Project] = [Project]()
     @Published var wits: [Wit] = [Wit]()
     @Published var fetchers: [Fetcher] = [Fetcher]()
@@ -303,64 +301,6 @@ class Fetcher: ObservableObject {
                     print("Fetcher count: \([info].count)")
                     self.wits = [info]
                     
-                }
-            }
-        }
-    }
-    
-    func getRelations(org: String, pat: String, email: String, witid: String, level: Int = 0) {
-        
-        if RELATIONS_LEVELS <= level {
-            return
-        }
-        
-        let header = buildHeader(pat: pat, email: email)
-        
-        self.isLoading = true
-        errorMessage = nil
-        
-        self.fetched.append(witid)
-        
-        self.wits.removeAll()
-        
-        let witBaseUrl: String = baseURL + org + "/_apis/wit/workitems/" + witid + "?$expand=relations"
-        
-        let url = NSURL(string: witBaseUrl)! as URL
-        
-        self.service.fetch(Wit.self, url: url, headers: header) { [unowned self] result in
-            
-            DispatchQueue.main.async {
-                
-                self.isLoading = false
-                
-                switch result {
-                case .failure(let error):
-                    if HTTP_ERROR {
-                        print("Fetcher error: \(error)")
-                    }
-                    self.errorMessage = error.localizedDescription
-                    
-                case .success(let info):
-                    if HTTP_DATA {
-                        print("Fetcher count: \([info].count)")
-                    }
-                    self.wit = info
-                    
-                    for r in self.wit.relations {
-                        let id: String = self.getWitNumber(url: r.url)
-                        let idNumber: Int = Int(id) ?? 0
-                        
-                        if idNumber > 50000 && idNumber < 500000 && !self.fetched.contains(id) {
-                            
-                            print("\(level) \(witid) -> \(idNumber)")
-                            
-                            let fetchR: Fetcher = Fetcher()
-                            
-                            fetchR.getRelations(org: org, pat: pat, email: email, witid: id, level: level + 1)
-                            
-                            self.fetchers.append(fetchR)
-                        }
-                    }
                 }
             }
         }
